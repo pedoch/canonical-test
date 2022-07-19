1b065ccef0c02b01ef9986cabc101468e59795ae
Pedro Ogheneochuko H <pedroogheneochuko@gmail.com>
2022-07-19 01:44:00 +0100

    finish assessment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  const getPost = async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch(
+        "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json"
+      );
+
+      const data = await response.json();
+
+      console.log(data);
+
+      setPosts(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  const formatDate = (date) => {
+    const newDate = new Date(date);
+
+    var months = {
+      1: "January",
+      2: "February",
+      3: "March",
+      4: "April",
+      5: "May",
+      6: "June",
+      7: "July",
+      8: "August",
+      9: "September",
+      10: "October",
+      11: "November",
+      12: "December",
+    };
+
+    return `${newDate.getDate()} ${
+      months[newDate.getMonth()]
+    } ${newDate.getFullYear()}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page__container">
+      <div className="row">
+        {loading && (
+          <div className="loader">
+            <i class="p-icon--spinner u-animation--spin"></i>
+          </div>
+        )}
+        {!loading &&
+          posts?.length > 0 &&
+          posts.map((post) => (
+            <div
+              key={post.id}
+              className="p-card shadow post-card col-4 col-medium-3"
+            >
+              <p className="u-no-margin padding-small">CLOUD AND SERVER</p>
+              <div className="p-card__content card-content padding-small separator-dotted">
+                <div>
+                  <img
+                    className="p-card__image"
+                    alt="feature media"
+                    src={post.featured_media}
+                  />
+                  <h4 title={post.title.rendered}>
+                    <a
+                      href={post.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {post.title.rendered}
+                    </a>
+                  </h4>
+                </div>
+                <div class="author">
+                  By{" "}
+                  <a
+                    href={post._embedded?.author[0]?.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {post._embedded?.author[0]?.name}
+                  </a>{" "}
+                  on {formatDate(post.date)}
+                </div>
+              </div>
+              <p className="u-no-margin padding-small separator-dotted p-text--small">
+                Article
+              </p>
+            </div>
+          ))}
+        {!loading && !posts?.length > 0 && <p>No posts available</p>}
+      </div>
     </div>
   );
 }
